9f79ece135ebf74028d3e85b69605be7f7c3e809
Jordi Albert Ribas González <jordiialb14@gmail.com>
2023-08-07 20:48:07 +0200

    Install UI library and add providers

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,22 +1,32 @@
 import { RealmProvider } from "@realm/react";
 import { Slot } from "expo-router";
 import { schemas } from "@src/models";
-import { SafeAreaView, StyleSheet } from "react-native";
-import colors from "@src/styles/colors";
+import { SafeAreaView, StyleSheet, useColorScheme } from "react-native";
+
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
 
 export default function RootLayout() {
+    const colorScheme = useColorScheme();
+    console.log(colorScheme);
+
     return (
-        <SafeAreaView style={styles.screen}>
-            <RealmProvider schema={schemas}>
-                <Slot />
-            </RealmProvider>
-        </SafeAreaView>
+        <>
+            <IconRegistry icons={EvaIconsPack} />
+            <SafeAreaView style={styles.screen}>
+                <RealmProvider schema={schemas}>
+                    <ApplicationProvider {...eva} theme={eva[colorScheme]}>
+                        <Slot />
+                    </ApplicationProvider>
+                </RealmProvider>
+            </SafeAreaView>
+        </>
     );
 }
 
 const styles = StyleSheet.create({
     screen: {
-      flex: 1,
-      backgroundColor: colors.darkBlue,
+        flex: 1
     },
-  });
+});
